ea542ca3c755155b07830028b02a1d8999e132ba
Carl-Johan Schnell <carl-johan.schnell@iver.se>
2025-09-23 08:38:04 +0200

    SDK 54 + Fixes

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,12 @@ import { Switch, Text, View } from 'react-native';
 export default function App() {
   const { colorScheme, toggleColorScheme } = useColorScheme();
   const isDarkMode = colorScheme === 'dark';
+
+  const handleSwitchChange = (value: boolean) => {
+    console.log('Switch value changed to:', value);
+    toggleColorScheme();
+  };
+
   return (
     <View className="flex-1 items-center justify-center bg-white dark:bg-neutral-900">
       <Text className="mb-1 text-xl font-mono text-black dark:text-white">
@@ -14,7 +20,7 @@ export default function App() {
       <Text className="mb-6 font-mono font-bold capitalize text-2xl text-black dark:text-white">
         {colorScheme} mode on
       </Text>
-      <Switch value={isDarkMode} onChange={toggleColorScheme} />
+      <Switch value={isDarkMode} onValueChange={handleSwitchChange} />
       <StatusBar style={isDarkMode ? 'light' : 'dark'} />
     </View>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/**/*.{js,jsx,ts,tsx}'
-  ],
-  presets: [
-    require('nativewind/preset')
-  ],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  darkMode: 'class', // 👈 allow manual toggling
+  presets: [require('nativewind/preset')],
   theme: {
-    extend: {}
+    extend: {},
   },
-  plugins: []
-}
+  plugins: [],
+};
